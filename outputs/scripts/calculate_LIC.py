344793e5598fdfc10fb6ab0c2bc62fef2bd4a22d
root-goksenin <121460943+root-goksenin@users.noreply.github.com>
2023-01-15 20:08:48 +0100

    Comment calculate_LIC.py

--- outputs/scripts/calculate_LIC.py
+++ outputs/scripts/calculate_LIC.py
@@ -5,10 +5,26 @@ from parse_LIC import LIC_Calculator
 
 @dataclass
 class Statistics:
+    '''
+    This class represent the statistics defined in the paper. Just mean and standard deviation.
+    '''
     mean: float       # 'a' has no default value
     std: float   # assign a default value for 'b'
 
 def calculate_statistics(data: LIC_Calculator) -> Dict[str,Statistics]:
+  '''
+  Calculate the statistics from the LIC_Calculator class defined. This function
+  returns a dictionary, which maps each model to statistics.
+  Attributes
+  ----------
+  data : LIC_Calculator
+      LIC_Calculator class for getting the models lic scores from the outputs
+  Returns
+  -------
+  Dict[str,Statistics]
+      Dictionary that maps each model 'str' into the statistics
+  
+  '''
   statistics = {}
   for index,model in enumerate(data.model_names.keys()):
     arr = np.array(data.LIC_scores[index])
@@ -16,11 +32,29 @@ def calculate_statistics(data: LIC_Calculator) -> Dict[str,Statistics]:
   return statistics
 
 def calculate_LIC(data_human: LIC_Calculator, data_generated: LIC_Calculator) -> Dict[str,float]:
+  '''
+  Calculate the LIC from human and generated annotations defined. This function
+  returns a dictionary, which maps each model to LIC.
+  Attributes
+  ----------
+  data_human : LIC_Calculator
+      LIC_Calculator class for getting the models lic scores from the outputs of the human
+      annotations.
+  data_generated : LIC_Calculator
+      LIC_Calculator class for getting the models lic scores from the outputs of the generated
+      annotations.
+  Returns
+  -------
+  Dict[str,Statistics]
+      Dictionary that maps each model 'str' into the LIC scores.
+  
+  '''
   LIC = {}
   assert data_human.model_names.keys() ==  data_generated.model_names.keys(), 'The data models do not contain the same models.'
   for index,model in enumerate(data_human.model_names.keys()):
     lic_M = np.array(data_generated.LIC_scores[index]).mean()
     lic_D = np.array(data_human.LIC_scores[index]).mean()
+    #Definition of the LIC from the paper. We substract the model's lic from the data's lic, to find how much bias is amplified.     
     LIC[model] = lic_M - lic_D
   return LIC
 
