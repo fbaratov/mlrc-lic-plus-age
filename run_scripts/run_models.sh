424c222e019c75d47eb56687c161201c3a1a7828
root-goksenin <goksenin.yuksel@outlook.com>
2023-01-18 19:46:28 +0100

    Finished training pre_trained bert

--- run_scripts/run_models.sh
+++ run_scripts/run_models.sh
@@ -65,9 +65,9 @@ echo -e "##########################################"
 
 #This script is responsible for running all the models with different seeds. 
 #We still need to find a way to write the output to file.
-models=('nic_equalizer')
+models=('oscar' 'nic_plus' 'nic_equalizer')
 #Run the models with only three seeds due to computational resources.
-seeds=(456)
+seeds=(0 12 456)
 
 # We set calc_model_leak when data is generated.
 # Otherwise calc_ann_leak
