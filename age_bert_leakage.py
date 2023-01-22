11735f08a5a84242651e6f347aaf6fb6be98afbd
Farrukh Baratov <farrukhsbaratov@gmail.com>
2023-01-22 01:03:29 +0100

    added bin_accuracy to age bert

--- age_bert_leakage.py
+++ age_bert_leakage.py
@@ -91,6 +91,15 @@ def get_parser():
 
     return parser
 
+def binary_accuracy(preds, y):
+    """
+    Returns accuracy per batch, i.e. if you get 8/10 right, this returns 0.8, NOT 8
+    """
+    #round predictions to the closest integer
+    rounded_preds = torch.round(torch.sigmoid(preds))
+    correct = (rounded_preds == y).float() #convert into float for division
+    acc = correct.sum() / len(correct)
+    return acc
 
 def calc_random_acc_score(args, model, test_dataloader):
     print("--- Random guess --")
