11ea561f5bd452f7ade823e84c5edd0ef0925906
Farrukh Baratov <farrukhsbaratov@gmail.com>
2023-01-22 01:08:37 +0100

    updated parser to gender bert parser

--- age_bert_leakage.py
+++ age_bert_leakage.py
@@ -51,40 +51,39 @@ from collections import namedtuple
 from age_utils import *
 
 def get_parser():
-    '''
-    Returns arguments parser. Barely modified, the only difference is that no "gender_or_race" arg is present.
-    '''
-
     parser = argparse.ArgumentParser()
     parser.add_argument("--task", default='captioning', type=str)
     parser.add_argument("--cap_model", default='sat', type=str)
-
+    parser.add_argument("--gender_or_race", default='gender', type=str)
     parser.add_argument("--calc_ann_leak", default=False, type=bool)
     parser.add_argument("--calc_model_leak", default=False, type=bool)
+    parser.add_argument("--calc_mw_acc", default=True, type=bool)
     parser.add_argument("--test_ratio", default=0.1, type=float)
     parser.add_argument("--balanced_data", default=True, type=bool)
-    parser.add_argument("--mask_age_words", default=True, type=bool)
-    parser.add_argument("--save_preds", default=False, type=bool)
-    parser.add_argument("--use_glove", default=False, type=bool)
-    parser.add_argument("--save_model_vocab", default=False, type=bool)
+    parser.add_argument("--mask_gender_words", default=True, type=bool)
+    parser.add_argument("--freeze_bert", default=False, type=bool)
+    parser.add_argument("--store_topk_gender_pred", default=False, type=bool)
+    parser.add_argument("--topk_gender_pred", default=50, type=int)
+    parser.add_argument("--calc_score", default=True, type=bool)
     parser.add_argument("--align_vocab", default=True, type=bool)
-    parser.add_argument("--mask_bias_source", default='', type=str, help='obj or person or both or none')
 
     parser.add_argument("--batch_size", default=64, type=int)
     parser.add_argument("--seed", default=0, type=int)
-    parser.add_argument("--num_epochs", default=20, type=int)
-    parser.add_argument("--learning_rate", default=5e-5, type=float)
-    parser.add_argument("--save_model", default=False, type=bool)
+    parser.add_argument("--num_epochs", default=5, type=int)
+    parser.add_argument("--learning_rate", default=1e-5, type=float)
+    parser.add_argument("--optimizer", default='adamw', type=str, help="adamw or adam")
+    parser.add_argument("--adam_correct_bias", default=True, type=bool)
+    parser.add_argument("--beta1", default=0.9, type=float)
+    parser.add_argument("--beta2", default=0.98, type=float, help="0.999:huggingface, 0.98:RoBERTa paper")
+    parser.add_argument("--adam_epsilon", default=1e-6, type=float, help="Epsilon for Adam optimizer. 1e-8:first, 1e-6:RoBERTa paper")
+    parser.add_argument("--weight_decay", default=0.1, type=float, help="Weight deay if we apply some. 0.001:first, 0.01:RoBERTa")
+    parser.add_argument("--coco_lk_model_dir", default='/Bias/leakage/', type=str)
     parser.add_argument("--workers", default=1, type=int)
 
-    parser.add_argument("--embedding_dim", default=100, type=int)
+    parser.add_argument("--max_seq_length", default=64, type=int)
     parser.add_argument("--hidden_dim", default=256, type=int)
     parser.add_argument("--output_dim", default=1, type=int)
-    parser.add_argument("--n_layers", default=2, type=int)
-    parser.add_argument("--bidirectional", default=True, type=bool)
     parser.add_argument("--dropout", default=0.5, type=float)
-    parser.add_argument("--pad_idx", default=0, type=int)
-    parser.add_argument("--fix_length", default=False, type=bool)
 
     return parser
 
@@ -375,7 +374,7 @@ if __name__ == "__main__":
     print("---Start---")
     print('Seed:', args.seed)
     print("Epoch:", args.num_epochs)
-    print("Freeze BERT:", args.freeze_bert)
+    #print("Freeze BERT:", args.freeze_bert)
     print("Learning rate:", args.learning_rate)
     print("Batch size:", args.batch_size)
     print("Calculate score:", args.calc_score)
